b950b0d1551465c607bf7be6509204f7d2239923
Tjq0911 <tujinqi0911@gmail.com>
2025-12-02 14:01:32 -0500

    step 1

---
+++ Coin.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Coin here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Coin extends Actor
+{
+    /**
+     * Act - do whatever the Coin wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        setLocation(getX(), getY() + 3);
+        if (isTouching(Snake.class))
+        {
+            Snake snake = (Snake) getOneIntersectingObject(Snake.class);
+            snake.catchCoin();
+            getWorld().removeObject(this);
+        }// Add your action code here.
+    }
+}
+
--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,32 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    private int score = 0;
+    private Snake snake;
+    private int time = 1800;
     public MyWorld() {
+         
         super(600, 400, 1);
+        Snake snake = new Snake();
+        addObject(snake, 300, 350);
+        addObject(new Coin(), Greenfoot.getRandomNumber(600), 0);
+        showText("Time: 30", 520, 20);
+        
     }
-}
+    public void act()
+    {
+        if(Greenfoot.getRandomNumber(100) < 2)
+        {
+            addObject(new Coin(), Greenfoot.getRandomNumber(600), 0);    
+        }
+        time--;
+        if(time % 60 == 0)
+        {
+            showText("Time: " + (time / 60), 520, 20);    
+        }
+        if(time == 0)
+        {
+            showText("Final score: " + score, 300, 200);
+            Greenfoot.stop();
+        }
+    } 
