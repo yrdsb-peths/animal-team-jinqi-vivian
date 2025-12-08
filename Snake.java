32ffd600c7811285d965222a32a8c198f9a6d881
Tjq0911 <tujinqi0911@gmail.com>
2025-12-08 09:41:09 -0500

    snake

--- Snake.java
+++ Snake.java
@@ -3,6 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Snake extends Actor{
 GreenfootSound elephantSound = new GreenfootSound("elephant-352698.mp3");
+GreenfootImage image = new GreenfootImage("snake idle.gif"); 
 
 
     /**
